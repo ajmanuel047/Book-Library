5162dd17138e742ed3ea8ac223a0bde70d858209
Unuafe Ajiri <aj4short@rocketmail.com>
2023-04-20 07:41:37 +0100

    create a form that when typed into and submitted, its content are shown on the page

--- file.js
+++ file.js
@@ -1,90 +1,106 @@
 /* eslint-disable no-plusplus */
+// eslint-disable-next-line no-unused-vars
 const cards = document.querySelector(".cards");
-const library = [
-  {
-    // eslint-disable-next-line linebreak-style
-    bookName: "Harry Potter",
-    pages: 345,
-    read: "Yes",
-  },
-  {
-    bookName: "Lord Of The Rings",
-    pages: 345,
-    read: "No",
-  },
-];
+const btn = document.querySelector(".btn");
+const form = document.querySelector("#forms");
+const display = document.querySelector(".display");
+const booktitle = document.querySelector("#book-title");
+const submit = document.querySelector("#submit");
 
-function Book(title, author, pages, read) {
-  // eslint-disable-next-line no-unused-expressions, no-sequences
-  (this.author = author),
-    (this.title = title),
-    (this.pages = pages),
-    (this.read = read),
-    // eslint-disable-next-line func-names
-    (Book.prototype.theHobbit = function () {
-      return `${this.title} by ${this.authorauthor}, ${this.pages} pages, ${this.read}`;
-    });
-}
+// eslint-disable-next-line no-unused-vars
 
-// card.style.color = 'green'
-
-// card.textContent = library[0].bookName
-function addBookToLibrary() {
-  for (let i = 0; i < library.length; i++) {
-    // eslint-disable-next-line linebreak-style
-
-    const div = document.createElement("div");
-    cards.appendChild(div);
-    div.classList.add("card");
-    div.classList.add(`card${i + 1}`);
-    const bookTitle = library[i].bookName;
-    const { pages } = library[i];
-    const readStatus = library[i].read;
-    const firstParagraph = document.createElement("p");
-    div.appendChild(firstParagraph);
-    firstParagraph.textContent = `Book Title : ${bookTitle}`;
-    const secondParagraph = document.createElement("p");
-    div.appendChild(secondParagraph);
-    secondParagraph.textContent = `Pages : ${pages}`;
-    const thirdParagraph = document.createElement("p");
-    div.appendChild(thirdParagraph);
-    thirdParagraph.textContent = `Read : ${readStatus}`;
-    // para1.textContent = `Book Title : ${text}`
-
-    // console.log(library[i])
-    // text = library[i].bookName
-    // text2 = library[i].pages
-    // text3 = library[i].read
-    // let para1 = document.createElement('p')
-    // cards.appendChild(para1)
-    // para1.textContent = `Book Title : ${text}`
-    // let para = document.createElement('p')
-    // cards.appendChild(para)
-    // para.textContent = `Pages : ${text2}`
-    // let para2 = document.createElement('p')
-    // cards.appendChild(para2)
-    // para2.textContent = text3
+submit.addEventListener("click", function (e) {
+  let library = [];
+  function Book(title, author, pages, read) {
+    // eslint-disable-next-line no-unused-expressions, no-sequences
+    (this.author = author),
+      (this.title = title),
+      (this.pages = pages),
+      (this.read = read),
+      // eslint-disable-next-line func-names
+      (Book.prototype.theHobbit = function () {
+        return `${this.title} by ${this.authorauthor}, ${this.pages} pages, ${this.read}`;
+      });
   }
-}
 
-addBookToLibrary();
+  let result = Array.from(document.querySelectorAll("#forms input")).reduce(
+    (acc, input) => ({ ...acc, [input.id]: input.value }),
+    {}
+  );
 
-// const Ajiri = new Book("The Hobbit", "J.R.R Tolkein", "295", "not read");
-// const Paul = new Book(
-//   "Harry Potter",
-//   "J.K Rowlings",
-//   "300",
-//   "finished reading"
-// );
-// console.log(Ajiri.theHobbit());
-// console.log(Paul.theHobbit());
+  let bookName = result["book-title"];
 
-// for (const prop in Ajiri) {
-//   console.log(Ajiri[prop]);
-//   //   card.textContent = `${Ajiri[prop]}`
-// }
+  let authorName = result["author"];
+  let pagesCount = result["pages"];
+  let readCount = result["read"];
+  const Ajiri = new Book(
+    `${bookName}`,
+    `${authorName}`,
+    `${pagesCount}`,
+    "Read"
+  );
+  library.push(Ajiri);
+
+  function displayBook() {
+    for (let i = 0; i < library.length; i++) {
+      // eslint-disable-next-line linebreak-style
+      if (
+        library[i].title == "" ||
+        library[i].author == "" ||
+        library[i].pages == "" ||
+        library[i].read == ""
+      ) {
+        console.log(i);
+        library.splice(i, 1);
+      } else {
+        const div = document.createElement("div");
+        cards.appendChild(div);
+        div.classList.add("card");
+        div.classList.add(`card${i + 1}`);
+        const bookTitle = library[i].title;
+        const { author } = library[i];
+        const { pages } = library[i];
+        const readStatus = library[i].read;
+        const firstParagraph = document.createElement("p");
+        div.appendChild(firstParagraph);
+        firstParagraph.textContent = `Book Title : ${bookTitle}`;
+        const authorParagraph = document.createElement("p");
+        div.appendChild(authorParagraph);
+        authorParagraph.textContent = `Author : ${author}`;
+        const secondParagraph = document.createElement("p");
+        div.appendChild(secondParagraph);
+        secondParagraph.textContent = `Pages : ${pages}`;
+        const thirdParagraph = document.createElement("p");
+        div.appendChild(thirdParagraph);
+        thirdParagraph.textContent = `Read : ${readStatus}`;
+        const button = document.createElement("button");
+        button.textContent = "Remove";
+        button.style.fontSize = "1rem";
+        div.appendChild(button);
+      }
+    }
+  }
+
+  console.log(library);
+  displayBook();
+
+  // console.log(library[0].title)
+  //  else {
+  //   for(let i = 1; i < library.length; i++){
+  //     if(library[i].library[i].title == ''){
+
+  //         library.splice(i, 1)
+  //         console.log(library[0].title)
+  //     }
+  //     }
+  // }
+});
+
 /*
 every book read
 every book unread
 when marked read remove from read and place in unread
+when submit is clicked create an array of random excited 
+words that would be displayed like "awesome", "sweetness" 
+etc
 */
